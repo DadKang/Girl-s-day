3b29df07e44cf396f28c5aeb8f3332f9b2725253
DadKang <59523614+DadKang@users.noreply.github.com>
2021-03-07 20:31:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "上点颜色",
+        bannar_coming: "好运莲莲~~",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
